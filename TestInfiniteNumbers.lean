5891d9c5f99488be792efe3f02bc32b09a7750fa
Nada Amin <namin@seas.harvard.edu>
2025-06-15 15:35:47 -0400

    add

---
+++ TestInfiniteNumbers.lean
@@ -0,0 +1,12 @@
+import LeanDisco
+import LeanDisco.Domains.InfiniteNumbers
+
+set_option maxHeartbeats 10000000
+
+open LeanDisco
+open LeanDisco.Domains.InfiniteNumbers
+
+def main : IO Unit := do
+  IO.println "Testing InfiniteNumbers Domain!"
+
+#eval runInfiniteNumbersDiscovery 25
